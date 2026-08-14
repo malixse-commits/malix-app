65a43f765b86549412955e03ff0f37ee929a4597
malixse-commits <malixse@gmail.com>
2026-08-14 20:43:49 +0200

    Add cloud sync configuration placeholder

---
+++ cloud-config.js
@@ -0,0 +1,6 @@
+// Publik konfiguration för En sak i taget.
+// Fylls i när Supabase-projektet är skapat. Använd endast den publika anon-nyckeln här.
+window.MALIX_CLOUD = window.MALIX_CLOUD || {
+  url: '',
+  anonKey: ''
+};
