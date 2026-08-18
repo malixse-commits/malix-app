1cc4b9ef813cce9baa89f6f964206e473a64321a
malixse-commits <malixse@gmail.com>
2026-08-18 05:54:09 +0200

    Remove obsolete dashboard cleanup patch

--- dashboard-cleanup.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  function removeDuplicateProduceCard(){
-    const produce=document.querySelector('#produceSummary');
-    const card=produce?.closest('.summary-card');
-    if(card) card.remove();
-  }
-  removeDuplicateProduceCard();
-  new MutationObserver(removeDuplicateProduceCard).observe(document.body,{childList:true,subtree:true});
-})();
