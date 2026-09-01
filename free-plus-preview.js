50f272c0091196bf471c4fea689273a391434243
malixse-commits <malixse@gmail.com>
2026-09-01 09:35:10 +0200

    Bump app module version after compass update

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260831-1200';
+  const VERSION='20260901-0915';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
