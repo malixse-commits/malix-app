cbe3067dd27eb5c4e5aa6442429c33a398362d4a
malixse-commits <malixse@gmail.com>
2026-09-01 10:56:35 +0200

    Load monthly cleaning reflection module

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260901-1005';
+  const VERSION='20260901-1025';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
@@ -65,6 +65,7 @@
       'movement-recovery.js',
       'cleaning-square.js',
       'cleaning-home-offer.js',
+      'cleaning-monthly.js',
       'cleaning-reflection-history-edit.js',
       'cleaning-flex-log.js',
       'diary-history.js'
