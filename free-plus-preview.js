936d4f50f818f49b7bf7091fd2089295e8643b81
malixse-commits <malixse@gmail.com>
2026-09-02 20:17:42 +0200

    Bump cache for monthly direction

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-2025';
+  const VERSION='20260902-2045';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
