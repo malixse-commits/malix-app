88baaf66d27856ca9e5c086a1189618883dfa925
malixse-commits <malixse@gmail.com>
2026-09-02 12:50:28 +0200

    Bump preview version for meal log visibility fix

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-1345';
+  const VERSION='20260902-1415';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
