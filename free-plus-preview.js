bdc960aad4d0777017d5c91209eec41403a4660a
malixse-commits <malixse@gmail.com>
2026-09-02 15:08:42 +0200

    Refresh PLUS kitchen deduction version

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-1810';
+  const VERSION='20260902-1818';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
