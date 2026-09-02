e69bb59ccef0f3758a5bbf609a082c43fb2734dd
malixse-commits <malixse@gmail.com>
2026-09-02 11:54:56 +0200

    Remove obsolete recipes-extra source

--- recipes-extra.js
+++
@@ -1,5 +0,0 @@
-// Kompatibilitetsfil för äldre index.html.
-// All receptdata finns nu i recipe-bank.js.
-// document.write används här endast medan denna klassiska script-tagg finns kvar i index.html,
-// så att recipe-bank.js laddas synkront innan nästa gamla script-tagg körs.
-document.write('<script src="recipe-bank.js?v=20260902-1210"><\/script>');
