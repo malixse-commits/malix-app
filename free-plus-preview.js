74a50884fc5a13c4dec9ecfb4f20e2cfbc94c46d
malixse-commits <malixse@gmail.com>
2026-09-02 15:25:15 +0200

    Bump cache for monthly presence history

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-1525';
+  const VERSION='20260902-1520';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
