64d8fcb2b9699b6c1d917769e289c46b4b33c930
malixse-commits <malixse@gmail.com>
2026-09-02 10:40:30 +0200

    Refresh meal registration integration

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-1040';
+  const VERSION='20260902-1055';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
