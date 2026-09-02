0861cb27cb6dee1354cd34dee6180d3d42432c1f
malixse-commits <malixse@gmail.com>
2026-09-02 12:33:12 +0200

    Refresh meal log module version

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-1235';
+  const VERSION='20260902-1315';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
