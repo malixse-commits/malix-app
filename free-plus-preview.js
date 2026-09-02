caa8a2ea274c81c034411ddd418237211bcbd567
malixse-commits <malixse@gmail.com>
2026-09-02 19:55:52 +0200

    Bump cache for monthly care histories

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-1550';
+  const VERSION='20260902-1954';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
