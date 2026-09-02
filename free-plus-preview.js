411c1f6410beacf5f7e059ff095d7b21598eaf51
malixse-commits <malixse@gmail.com>
2026-09-02 13:17:24 +0200

    Refresh PLUS kitchen integration version

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260902-1715';
+  const VERSION='20260902-1745';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
