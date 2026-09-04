4894f17e2d5e5fbe9c854d655ac0ff0f00e52698
malixse-commits <malixse@gmail.com>
2026-09-04 08:23:31 +0200

    Bump cache after restoring stable meal log

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260904-0840';
+  const VERSION='20260904-0900';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
