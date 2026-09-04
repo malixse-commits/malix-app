16b24b76bf228cd3eada6045266f734407bc22f2
malixse-commits <malixse@gmail.com>
2026-09-04 08:33:58 +0200

    Bump cache for corrected meal save flow

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260904-0900';
+  const VERSION='20260904-0930';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
