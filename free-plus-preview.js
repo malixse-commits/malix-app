add4ce5adb3fcf8b56c4bf87cd479708ca40530e
malixse-commits <malixse@gmail.com>
2026-09-05 16:41:24 +0200

    Refresh self-care training module cache

--- free-plus-preview.js
+++ free-plus-preview.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260905-1630';
+  const VERSION='20260905-1640';
   const loaded = new Set(Array.from(document.scripts).map(s => s.getAttribute('src')).filter(Boolean));
 
   function loadScript(src) {
